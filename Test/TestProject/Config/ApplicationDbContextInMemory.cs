3f9601a90aa3f091d78aef01b39e132b25391183
carloscordobanvc <89094085+carloscordobanvc@users.noreply.github.com>
2022-04-19 13:40:29 -0500

    get all user test and test project

---
+++ Test/TestProject/Config/ApplicationDbContextInMemory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserWebApp.DBModels;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using UserWebApp.Services.Users;
+
+namespace UserWebAppTest.Config
+{
+    public static class ApplicationDbContextInMemory
+    {
+        public static App_DBContext Get()
+        {
+            var options = new DbContextOptionsBuilder<App_DBContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
+            return new App_DBContext(options);
+        }
+        public static IMapper GetMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                options.AddProfile(new UserProfile());
+            });
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ Test/TestProject/UserServiceTest.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using UserWebApp.DBModels;
+using UserWebApp.Services.Users;
+using UserWebAppTest.Config;
+
+namespace UserWebAppTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+
+        [TestMethod]
+        public async Task TryToGetAllUsers()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+            var mapper = ApplicationDbContextInMemory.GetMapper();
+            context.Add(new User()
+            {
+                FirstName = "Carlos",
+                FirstSurname = "Cordoba",
+                Email = "carlosm.cordobae@gmail.com",
+                Cellphone = "3013214435",
+                RoleId = 1
+            });
+            context.SaveChanges();
+            var context2 = ApplicationDbContextInMemory.Get();
+            var userService = new UserService(context2, mapper);
+            var response = await userService.GetAllUsers();
+            Assert.AreEqual(1, response.Count());
+        }
+    }
+}
