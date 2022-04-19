c401d5c7fa84f24bc39e5cf00b441b9faa52c409
carloscordobanvc <89094085+carloscordobanvc@users.noreply.github.com>
2022-04-19 14:01:50 -0500

    update user test

--- Test/TestProject/UserServiceTest.cs
+++ Test/TestProject/UserServiceTest.cs
@@ -76,5 +76,36 @@ namespace UserWebAppTest
             var response = await context.Users.ToArrayAsync();
             Assert.AreEqual(0, response.Count());
         }
+        [TestMethod]
+        public async Task TryToModifyUser()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+            var mapper = ApplicationDbContextInMemory.GetMapper();
+            var user = new User()
+            {
+                FirstName = "Carlos",
+                FirstSurname = "Cordoba",
+                Email = "carlosm.cordobae@gmail.com",
+                Cellphone = "3013214435",
+                RoleId = 1
+            };
+            context.Add(user);
+            context.SaveChanges();
+            var userId = user.UserId;
+            var user2 = new UserDto()
+            {   
+                UserId = userId,
+                FirstName = "Mauro",
+                FirstSurname = "Cordoba",
+                Email = "carlosm.cordobae@gmail.com",
+                Cellphone = "3013214435",
+                RoleId = 1
+            };
+            var context2 = ApplicationDbContextInMemory.Get();
+            var userService = new UserService(context2, mapper);
+            await userService.UpdateUser(user2);
+            var finalUser = await context2.Users.FirstOrDefaultAsync();
+            Assert.AreEqual(finalUser.FirstName, user2.FirstName);
+        }
     }
 }
