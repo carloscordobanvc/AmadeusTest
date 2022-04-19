9da42a11ef9736c51a690b4ddf600c435057cb15
carloscordobanvc <89094085+carloscordobanvc@users.noreply.github.com>
2022-04-19 13:44:16 -0500

    Create user test

--- Test/TestProject/UserServiceTest.cs
+++ Test/TestProject/UserServiceTest.cs
@@ -32,5 +32,25 @@ namespace UserWebAppTest
             var response = await userService.GetAllUsers();
             Assert.AreEqual(1, response.Count());
         }
+        [TestMethod]
+        public async Task TryToCreateUser()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+            var mapper = ApplicationDbContextInMemory.GetMapper();
+            var user =  new UserDto()
+            {
+                FirstName = "Carlos",
+                FirstSurname = "Cordoba",
+                Email = "carlosm.cordobae@gmail.com",
+                Cellphone = "3013214435",
+                RoleId = 1
+            };
+            context.SaveChanges();
+            var context2 = ApplicationDbContextInMemory.Get();
+            var userService = new UserService(context2, mapper);
+            await userService.CreateUser(user);
+            var response = await context.Users.ToArrayAsync();
+            Assert.AreEqual(1, response.Count());
+        }
     }
 }
